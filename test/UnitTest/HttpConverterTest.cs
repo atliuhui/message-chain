9096644c54aa7b667b30bab3767c71dfce7fe0a3
LIU Hui <atliuhui@outlook.com>
2026-05-07 12:56:37 +0800

    Fix the sample file path in HttpConverterTest

--- test/UnitTest/HttpConverterTest.cs
+++ test/UnitTest/HttpConverterTest.cs
@@ -10,7 +10,7 @@ public sealed class HttpConverterTest
     [TestCategory("CI")]
     public async Task Parse_Request()
     {
-        var raw = File.ReadAllText(@"examples/example-request.http", Encoding.UTF8);
+        var raw = File.ReadAllText(@"examples/httpbin-request.http", Encoding.UTF8);
         var request = HttpRequestMessageConverter.Parse(raw);
 
         var client = new HttpClient();
@@ -23,7 +23,7 @@ public sealed class HttpConverterTest
     [TestCategory("CI")]
     public async Task Parse_Response()
     {
-        var raw = File.ReadAllText(@"examples/example-response.http", Encoding.UTF8);
+        var raw = File.ReadAllText(@"examples/httpbin-response.http", Encoding.UTF8);
         var response = HttpResponseMessageConverter.Parse(raw);
 
         Assert.IsTrue(response.IsSuccessStatusCode);
